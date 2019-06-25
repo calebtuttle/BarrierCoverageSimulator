85694d94ca2fbf91a2bc4393398ddf7f4dbcaa4d
Caleb <caleb.tuttle@pop.belmont.edu>
2019-06-25 12:08:39 -0500

    Started greedy algorithm

--- src/BarrierCoverageSimulator.java
+++ src/BarrierCoverageSimulator.java
@@ -27,6 +27,10 @@ public class BarrierCoverageSimulator {
     public BarrierCoverageSimulator()
     {
         planarRegion = getPlanarRegion();
+        placeLines();
+//        placeRandomly();
+        placeGreedy();
+        System.out.println();
         print2DArray(planarRegion);
     }
 
@@ -44,8 +48,8 @@ public class BarrierCoverageSimulator {
         gen = new Random();
 
         for (int i = 0; i < 10; i++) {
-            int x = gen.nextInt(11);
-            int y = gen.nextInt(16);
+            int x = gen.nextInt(10);
+            int y = gen.nextInt(15);
 
             coordinates[i][0] = x;
             coordinates[i][1] = y;
@@ -54,33 +58,122 @@ public class BarrierCoverageSimulator {
         return coordinates;
     }
 
-
     /**
-     * Place cameras in the planar region according
-     * to the random algorithm.
+     * Place cameras (denoted by the char 'C') in the
+     * planar region according to the random algorithm by
+     * modifying planarRegion to have a 'C' wherever a camera
+     * is placed.
      */
     public void placeRandomly()
     {
-//        for (int)
+        int[][] coordinates = getRandomCoordinates();
+
+        for (int i = 0; i < coordinates.length; i++) {
+            int x = coordinates[i][0];
+            int y = coordinates[i][1];
+
+            planarRegion[y][x] = 'C';
+        }
+    }
+
+
+    /**
+     * Get coordinates from greedy algorithm
+     * @return
+     */
+    public int[][] getGreedyCoordinates()
+    {
+        int[][] coordinates = new int[10][2];
+        gen = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            int x = gen.nextInt(6) + 2; // Restrict bounds
+            int y = gen.nextInt(6) + 2;
+
+            coordinates[i][0] = x;
+            coordinates[i][1] = y;
+        }
+
+
+        return coordinates;
+    }
+
+    /**
+     * Place cameras in the planar region according to
+     * the greedy algorithm by modifying planarRegion
+     * to have a 'C' wherever a camera is placed.
+     */
+    public void placeGreedy()
+    {
+        int[][] coordinates = getGreedyCoordinates();
+
+        for (int i = 0; i < coordinates.length; i++) {
+            int x = coordinates[i][0];
+            int y = coordinates[i][1];
+
+            planarRegion[y][x] = 'C';
+        }
     }
 
 
     /**
      * Get the planar region to be covered. Dimensions of region:
      * 15 high by 10 wide.
-     * @return a 2D array representing the coverable planar region.
+     * @return a 2D array of chars representing the coverable planar region.
      */
     public char[][] getPlanarRegion()
     {
         char[][] planarRegion = new char[15][10];
+
+        for (int y = 0; y < planarRegion.length; y++) {
+            for (int x = 0; x < planarRegion[y].length; x++) {
+                planarRegion[y][x] = '.';
+            }
+        }
+
         return planarRegion;
     }
 
+    /**
+     * Place the barrier lines in the planar region.
+     * TODO: Determine where the lines should actually be
+     */
+    public void placeLines()
+    {
+        for (int y = 0; y < planarRegion.length; y++) {
+            for (int x = 0; x < planarRegion[y].length; x++) {
+                // Place the top line
+                if (x > 1 && x < 8 && y == 2) {
+                    planarRegion[y][x] = '-';
+                }
+
+                // Place the bottom line
+                if (x > 1 && x < 8 && y == 7) {
+                    planarRegion[y][x] = '-';
+                }
+
+                // Place the left line
+                if (x == 2 && y > 1 && y < 8) {
+                    planarRegion[y][x] = '|';
+                }
+
+                // Place the right line
+                if (x == 7 && y > 1 && y < 8) {
+                    planarRegion[y][x] = '|';
+                }
+            }
+        }
+    }
+
+    /**
+     * Print the given 2D array.
+     * @param array the 2D array to be printed.
+     */
     public void print2DArray(char[][] array)
     {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + " ");
+        for (int y = 0; y < array.length; y++) {
+            for (int x = 0; x < array[y].length; x++) {
+                System.out.print(array[y][x] + " ");
             }
             System.out.println();
         }
