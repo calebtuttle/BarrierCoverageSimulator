4957ea244b0d664dab861b9a050fbc88abe14de3
Caleb <caleb.tuttle@pop.belmont.edu>
2019-06-25 09:58:01 -0500

    Initial

---
+++ src/BarrierCoverageSimulator.java
@@ -0,0 +1,35 @@
+/**
+ * BarrierCoverageSimulator simulates the 2D planar region
+ * with the barrier lines that need to be covered as well as
+ * camera placement. Camera placement is determined by a
+ * random baseline algorithm, greedy algorithm, or heuristic
+ * algorithm.
+ *
+ * @author Caleb Tuttle
+ * @version 6-25-2019
+ */
+public class BarrierCoverageSimulator {
+
+    public static void main(String[] args)
+    {
+        BarrierCoverageSimulator sim = new BarrierCoverageSimulator();
+    }
+
+    public BarrierCoverageSimulator()
+    {
+        char[][] planarRegion = getPlanarRegion();
+    }
+
+    public char[][] getPlanarRegion()
+    {
+        char[][] planarRegion = new char[15][10];
+
+
+        return planarRegion;
+    }
+
+    public void print2DArray(char[][] array)
+    {
+
+    }
+}
